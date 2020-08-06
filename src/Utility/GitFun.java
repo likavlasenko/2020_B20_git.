834e9c994588f084d5bc437dd7914bed99734d3b
Anzhelika Vlasenko <68965329+likavlasenko@users.noreply.github.com>
2020-08-05 20:37:22 -0500

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -6,6 +6,12 @@ import org.w3c.dom.ls.LSOutput;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //GitHub new line
+        
+        
+        // * - * 
+                 // 0_0
+        // o_O
 
     }
 }
