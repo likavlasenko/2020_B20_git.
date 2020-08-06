469cdd9d064d65e1a9a45b736258c4b4f042d1c2
likavlasenko <likacherie@gmail.com>
2020-08-05 20:29:42 -0500

    new

---
+++ src/Utility/GitFun.java
@@ -0,0 +1,11 @@
+package Utility;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class GitFun {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
